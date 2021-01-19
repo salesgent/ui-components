b96106bd3e62ab467678017d5b368b18ed30c908
savanchhayani <savanchhayani@gmail.com>
2021-01-19 10:32:37 +0530

    Display elements of the variant.

--- src/components/product/subComponents/two/two.jsx
+++ src/components/product/subComponents/two/two.jsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ShoppingCartOutlined } from '@ant-design/icons';
+
+import { Button } from 'antd';
+import styled from 'styled-components';
+import { ProductContext } from '../../product';
+import { preventOuterClick } from '../../../../utils/commons';
+
+const StyledTwo = styled.div`
+  min-height: 50px;
+`;
+const StyledPrice = styled.div``;
+const StyledOutOfStock = styled.div``;
+const StyledAddToCartButton = styled(Button)``;
 
 function Two() {
-  return <div>Second variant</div>;
+  const {
+    item: { price, currency, qty, id },
+    addToCart,
+  } = useContext(ProductContext);
+  const amount = `${currency} ${price}`;
+  const isProductOutOfStock = qty === 0;
+
+  const onAddToCartClick = (e) => {
+    preventOuterClick(e);
+    addToCart({ id, qty: 1 });
+  };
+
+  return (
+    <StyledTwo>
+      <StyledPrice>{amount}</StyledPrice>
+      {isProductOutOfStock ? (
+        <StyledOutOfStock>out of stock</StyledOutOfStock>
+      ) : (
+        <StyledAddToCartButton
+          type="primary"
+          icon={<ShoppingCartOutlined />}
+          onClick={onAddToCartClick}
+        >
+          Add to Cart
+        </StyledAddToCartButton>
+      )}
+    </StyledTwo>
+  );
 }
 
 export default Two;
